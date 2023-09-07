994d286490209802d6cec2dca9555fbe011a953f
Matheus Gabriel <matheusgmello1@gmail.com>
2023-09-07 19:52:58 -0300

    feat: create UserDTO

--- src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
+++ src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
@@ -2,10 +2,7 @@ package com.picpaysimplificado.domain.transaction;
 
 import com.picpaysimplificado.domain.user.User;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -15,15 +12,18 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private BigDecimal amount;
+
     @ManyToOne
     @JoinColumn(name = "sender_id")
     private User sender;
+
     @ManyToOne
     @JoinColumn(name = "receiver_id")
     private User receiver;
